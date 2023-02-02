ea74f44873b5e87218d66bea8fdf64fb7ed6da85
moonjongjs <moonseonjong@naver.com>
2023-02-03 05:03:02 +0900

    Typescript React kurly

---
+++ src/components/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function App(){
+    return(
+        <div>
+            <h1>앱 컴포넌트</h1>
+        </div>
+    )
+}
---
+++ src/components/MainModalComponent.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function MainModalComponent() {
+
+    return (
+        <div>
+            <h1>MainModalComponent</h1>
+        </div>
+    );
+};
